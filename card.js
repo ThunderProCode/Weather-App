1a4b198cc4c152214e81561a9e5c7a29116b622b
Hector Eddin Acosta Najera <hector889ju@gmail.com>
2022-03-24 14:26:59 -0600

    flexbox bug fixed

--- card.js
+++ card.js
@@ -1,4 +1,4 @@
-const cityUrl = "https://api.openweathermap.org/geo/1.0/direct?q=";
+const cityUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
 const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?";
 const apiKey = "c02496050420768f1e798238df0de32c";
 
@@ -115,7 +115,7 @@ const card = async (cityName) => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-wind" viewBox="0 0 16 16">
                     <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5zm-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2zM0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5z"/>
                 </svg>
-                <p>${weatherData[3]} k/hr</p>
+                <p>${weatherData[3]} m/s</p>
             </div>
         </div>
     `;
